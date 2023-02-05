9458bd6dfb7c42698be22fc4fc9a162d3bded884
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-05 20:15:54 -0300

    Create ex015.js

---
+++ aula13/ex015.js
@@ -0,0 +1,12 @@
+var escreva = 'Olá, tudo bem?'
+var cont = 1
+
+// while (cont <= 3){
+//   console.log(`Passo ${cont}`)
+//   cont ++
+// }
+
+do{ 
+  console.log(`Passo ${cont}`)
+  cont ++
+} while (cont <= 3)
