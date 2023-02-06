91205cdbef4bea84106084c9d57722acae99265b
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-05 22:38:02 -0300

    Update ex014.js

--- aula12/ex014.js
+++ aula12/ex014.js
@@ -39,3 +39,49 @@ switch (diaSem){
     console.log('[ERRO] Dia inválido!')
     break
 }
+
+// Treinando mais uma vez
+// var agora = new Date()
+// var diaSem = agora.getDate()
+
+// switch (diaSem){
+//   case 0:
+//     console.log('Domingo')
+//     break
+//   case 1:
+//     console.log('Segunda')
+//     break
+//   case 2:
+//     console.log('Terça')
+//     break
+//   case 3:
+//     console.log('Quarta')
+//     break
+//   case 4:
+//     console.log('Quinta')
+//     break
+//   case 5:
+//     console.log('Sexta')
+//     break
+//   case 6:
+//     console.log('Sábado')
+//     break
+//   default:
+//     console.log('[ERRO] Dia inválido')
+//     break
+// }
+
+// Praticando
+
+// var agora = new Date()
+// var horas = agora.getHours()
+
+// if (horas >= 5 && horas < 12){
+//   console.log('Bom dia!')
+// } else if (horas >= 12 && horas < 18){
+//   console.log('Boa Tarde!')
+// } else if (horas >= 18 && horas < 24){
+//   console.log('Boa noite!')
+// } else{
+//   console.log('Boa madrugada!')
+// }
