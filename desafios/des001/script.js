27f5ff85c8b40838d828b361cc47ba768f802f29
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-06 16:26:48 -0300

    Finalizando script

--- desafios/des001/script.js
+++ desafios/des001/script.js
@@ -1,1 +1,19 @@
-window.alert('Olá')
+function carregar() {
+  var msg = window.document.querySelector("div#msg");
+  var img = window.document.querySelector("img#imagem");
+  var agora = new Date();
+  var hora = agora.getHours();
+  if (hora >= 0 && hora < 12) {
+    msg.innerText = `Agora são ${hora} horas, tenha uma boa manhã.`;
+    img.src = 'manha.jpg'
+    document.body.style.background = '#e2cd9f'
+  } else if (hora >= 12 && hora < 18) {
+    msg.innerText = `Agora são ${hora} horas, tenha uma boa tarde.`;
+    img.src = 'tarde.jpg';
+    document.body.style.background = '#b9846f'
+  } else {
+    msg.innerText = `Agora são ${hora} horas, tenha uma boa noite.`;
+    img.src = 'noite.jpg';
+    document.body.style.background = '#515154'
+  }
+}
