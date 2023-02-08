f59acec2649ae0c565f908598d5bc79abc2e9390
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-08 09:12:31 -0300

    Concluindo

--- aula12-desafios/des002/script.js
+++ aula12-desafios/des002/script.js
@@ -1,6 +1,8 @@
 function verificar() {
   // conteúdo
   var content = document.querySelector("div#conteudo");
+  var img = document.createElement("img");
+  img.setAttribute("id", "foto");
 
   // Idade
   var anon = document.querySelector("input#iano").value;
@@ -10,24 +12,48 @@ function verificar() {
   var idade = anoa - anon;
 
   // sexo
-  var inputh = document.querySelector("input[value='h']");
-  var inputm = document.querySelector("input[value='m']")
-  var sexo;
-  
-  inputh.addEventListener("change", function(){
-    if (inputh.checked){
-      sexo = inputh.value
-    }
-  })
-  inputm.addEventListener("change", function (){
-    if (inputm.checked){
-      sexo = inputm.value
-    }
-  })
+  sexo = document.getElementsByName("gen");
+  if (sexo[0].checked) {
+    sexo = "h";
+  } else {
+    sexo = "m";
+  }
 
   // Estrutura Condicional
-  if (idade <= 13 && sexo == 'm') {
+  if (idade == 0){
+    content.innerHTML = 'Coloque a idade corretamente'
+  } else if (idade <= 13 && sexo == "m") {
     content.innerText = `Detectamos uma menina de ${idade} anos.`;
-    content += Img.src('crianca-m')
+    img.setAttribute("src", "imagens/crianca-m.jpg");
+    content.appendChild(img);
+  } else if (idade <= 21 && sexo == 'm'){
+    content.innerText = `Detectamos uma moça de ${idade} anos.`;
+    img.setAttribute("src", "imagens/adolescente-m.jpg");
+    content.appendChild(img);
+  } else if (idade <= 50 && sexo == 'm'){
+    content.innerText = `Detectamos uma mulher de ${idade} anos.`;
+    img.setAttribute("src", "imagens/adulto-m.jpg");
+    content.appendChild(img);
+  } else if (idade > 50 && sexo == 'm'){
+    content.innerText = `Detectamos uma senhora de ${idade} anos.`;
+    img.setAttribute("src", "imagens/idoso-m.jpg");
+    content.appendChild(img);
+  } else if (idade <= 13){
+    content.innerText = `Detectamos um menino de ${idade} anos.`;
+    img.setAttribute("src", "imagens/crianca-h.jpg");
+    content.appendChild(img);
+  } else if (idade <= 21){
+    content.innerText = `Detectamos um jovem de ${idade} anos.`;
+    img.setAttribute("src", "imagens/adolescente-h.jpg");
+    content.appendChild(img);
+  } else if (idade <= 50){
+    content.innerText = `Detectamos um homem de ${idade} anos.`;
+    img.setAttribute("src", "imagens/adulto-h.jpg");
+    content.appendChild(img);
+  } else{
+    content.innerText = `Detectamos um senhor de ${idade} anos.`;
+    img.setAttribute("src", "imagens/idoso-h.jpg");
+    content.appendChild(img);
   }
+  
 }
