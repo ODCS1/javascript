0cdbbabfe73cc63695095afea646a7749eb2a96e
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-07 22:46:12 -0300

    imagens

--- aula12-desafios/des002/script.js
+++ aula12-desafios/des002/script.js
@@ -1,19 +1,33 @@
-function verificar(){
-
+function verificar() {
   // conteúdo
-  var content = document.querySelector('div#conteudo')
+  var content = document.querySelector("div#conteudo");
 
   // Idade
-  var anon = document.querySelector('imput#iano')
-  var anon = Number(anon)
-  var agora = new Date()
-  var anoa = agora.getFullYear()
-  var idade = anoa - anon
+  var anon = document.querySelector("input#iano").value;
+  var anon = Number(anon);
+  var agora = new Date();
+  var anoa = agora.getFullYear();
+  var idade = anoa - anon;
 
   // sexo
-  var sexo = document.querySelector('input#')
+  var inputh = document.querySelector("input[value='h']");
+  var inputm = document.querySelector("input[value='m']")
+  var sexo;
+  
+  inputh.addEventListener("change", function(){
+    if (inputh.checked){
+      sexo = inputh.value
+    }
+  })
+  inputm.addEventListener("change", function (){
+    if (inputm.checked){
+      sexo = inputm.value
+    }
+  })
 
-  if (idade <= 13) {
-    content.innerText = `Detectamos`
+  // Estrutura Condicional
+  if (idade <= 13 && sexo == 'm') {
+    content.innerText = `Detectamos uma menina de ${idade} anos.`;
+    content += Img.src('crianca-m')
   }
 }
