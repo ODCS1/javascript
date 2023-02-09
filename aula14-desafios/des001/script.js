cdaebed1de5c633c6122efbdde539cc9df6dc062
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-09 14:26:31 -0300

    Finalizado js exercício

--- aula14-desafios/des001/script.js
+++ aula14-desafios/des001/script.js
@@ -7,20 +7,28 @@ function contar() {
   let fim = document.querySelector("input#iend");
   let passo = document.querySelector("input#ipasso");
 
-  if (i.length == 0 || f.length == 0 || p.length == 0) {
+  // Estruturas que vão ter um resultado na tela
+
+  if (inicio.value == 0 && fim.value == 0 || passo.value == 0) {
     window.alert("[ERRO] tente novamente");
   } else {
     msg.innerHTML = "Contando: ";
     let i = Number(inicio.value);
-    let f = Number(end.value);
+    let f = Number(fim.value);
     let p = Number(passo.value);
     if (i < f && p > 0) {
       for (var c = i; c <= f; c += p) {
         msg.innerHTML += `${c} \u{1F449}`;
+        if (c == f){
+          msg.innerHTML += ` FIM! \u{1F3C1}`;
+        }
       }
     } else if (i > f) {
       for (c = i; c >= f; c -= p) {
         msg.innerHTML += `${c} \u{1F449}`;
+        if (c == f){
+          msg.innerHTML += ` FIM! \u{1F3C1}`;
+        }
       }
     }
   }
