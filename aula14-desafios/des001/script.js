d3798c6aa5fea8681305a488f517252d847396e8
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-08 21:48:31 -0300

    Atualização

--- aula14-desafios/des001/script.js
+++ aula14-desafios/des001/script.js
@@ -10,16 +10,18 @@ function contar() {
   var f = Number(end.value);
   var p = Number(passo.value);
 
-  if (i == 0 || f == 0 || p == 0){
-    msg.innerHTML = '[ERRO] tente novamente'
-  } else{
-    msg.innerHTML = 'Contando: '
-    if (i < f && p > 0){
-      for (var c = i; c <= f; c += p){
-        msg.innerHTML += `${c} \u{1F449}`
+  if (i == 0 || f == 0 || p == 0) {
+    msg.innerHTML = "[ERRO] tente novamente";
+  } else {
+    msg.innerHTML = "Contando: ";
+    if (i < f && p > 0) {
+      for (var c = i; c <= f; c += p) {
+        msg.innerHTML += `${c} \u{1F449}`;
       }
-    } else{
-      if (i > f && p < 0)
+    } else if (i > f) {
+      for (c = i; c >= f; c -= p) {
+        msg.innerHTML += `${c} \u{1F449}`;
+      }
     }
   }
 }
