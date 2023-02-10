6e128bd032c9b3689546c5f09b5407f96ff4bc67
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-10 16:15:18 -0300

    Update script.js

--- aula14-desafios/des002/script.js
+++ aula14-desafios/des002/script.js
@@ -2,13 +2,14 @@ document.getElementById("botao").onclick = function () {
   let num = document.querySelector("input#itxt");
   let n = Number(num.value);
   let res = document.querySelector("select#seltab");
-  if (n.length == 0) {
-    window.alert = "[ERRO] Coloque um valor válido";
+  if (num.value.length == 0) {
+    window.alert("[ERRO] Coloque um valor válido");
   } else{
     res.innerHTML = ''
     for (var cont = 1; cont <= 10; cont++){
       let item = document.createElement('option')
       item.innerText = `${n} x ${cont} = ${n*cont}`
+      item.value = 'res${cont}'
       res.appendChild(item)
     }
   }
