052c1bf9faa913594cc5b6ef6827bc67e2384fd7
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-11 17:47:14 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,9 +1,17 @@
 let v = [3, 5, 8, 3, 9];
 
-v.sort();
 v.push(1);
+v.sort();
 console.log(v);
 console.log(`O vetor tem ${v.length} posições`);
 console.log(`O valor do vetor na primeira posição é ${v[0]}.`);
 console.log(`O valor do vetor na última posição é ${v[v.length - 1]}.`);
 console.log(v);
+
+let pos = v.indexOf(8);
+
+if (pos == -1) {
+  console.log("Ovalaor não foi encontrado");
+} else {
+  console.log(`O valor está na posição ${pos}`);
+}
