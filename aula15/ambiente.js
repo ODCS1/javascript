bef8cbeea4501454eabfb602a3e6acdf3acb6fc8
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-11 17:29:46 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let v = [3, 5, 8, 3, 9];
+
+v.sort();
+v.push(1);
+console.log(v);
+console.log(`O vetor tem ${v.length} posições`);
+console.log(`O valor do vetor na primeira posição é ${v[0]}.`);
+console.log(`O valor do vetor na última posição é ${v[v.length - 1]}.`);
+console.log(v);
