fc6dcec143b2d6526d7d210b894f82ab201dc340
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2023-02-19 17:33:24 -0300

    p003

---
+++ pratica/p003/script.js
@@ -0,0 +1,62 @@
+num = 4
+
+switch (num){
+    case 0:
+        console.log('Número vale 0')
+        break
+    case 1:
+        console.log('Número vale 1')
+        break
+    case 2:
+        console.log('Número vale 2')
+        break
+    case 3:
+        console.log('Número vale 3')
+        break
+    case 4:
+        console.log('Número vale 4')
+        break
+    case 5:
+        console.log('Número vale 5')
+        break
+    case 6:
+        console.log('Número vale 6')
+        break
+    case 7:
+        console.log('Número vale 7')
+        break
+    case 8:
+        console.log('Número vale 8')
+        break
+    case 9:
+        console.log('Número vale 9')
+        break
+    case 10:
+        console.log('Número vale 10')
+        break
+}
+
+function clicar(){
+    console.log('Clicou')
+}
+
+
+let c = 1
+while (c <= 7){
+    clicar()
+}
+
+/*  Outras opções de estruturas de repetição:
+
+    1°: (Utilizando o do-while)
+
+    do{
+        clicar()
+    }while(c <= 7)
+
+    2°: (Utilizando o for)
+
+    for(let cont = 1; cont <= 7; cont++){
+        clicar()
+    }
+*/
