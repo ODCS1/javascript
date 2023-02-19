6d56925722d2c104935d29a8a5389eb219c62c38
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2023-02-19 17:09:12 -0300

    p001

---
+++ pratica/p001/script.js
@@ -0,0 +1,24 @@
+let nome = Joestars
+
+console.log(`O nome é ${nome}. Seja bem vindo!`)
+
+if (nome == 'undefined'){
+    console.log('O seu nome não está definido')
+} else{
+    console.log(`Nome definido como ${nome}`)
+}
+
+for (let c = 1; c <= 5; c++){
+    console.log(c)
+}
+
+let cont = 1
+while (cont <= 7){
+    console.log(cont)
+}
+
+
+let conta = 1
+do{
+    console.log(conta)
+} while(conta)
