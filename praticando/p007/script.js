268d81a3144f745fce59781599a9539bb29d50ea
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2023-02-21 01:46:01 -0300

    p007

---
+++ praticando/p007/script.js
@@ -0,0 +1,15 @@
+let idk = 9
+let rtx = 9 * 2(idk - 5)
+if (rtx < 20){
+    if(rtx >10){
+        for(let c = 1; c <=5; c++){
+            console.log('Teste', c)
+        }
+    }else{
+        for(let c = 1; c <= 5; c++){
+            console.log(c, 'Teste')
+        }
+    }
+} else{
+    console.log('Ok')
+}
