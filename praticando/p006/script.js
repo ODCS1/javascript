e73ad825e44dd91df41d79540a5ad41d42ae4a42
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2023-02-20 23:01:25 -0300

    p006

---
+++ praticando/p006/script.js
@@ -0,0 +1,13 @@
+let num = 17
+console.log(num)
+for (let c = 1; c <= num; c++){
+    console.log(c)
+}
+
+if(num <= 15){
+    console.log('idk')
+}else if(num > 15 && num < 20){
+    console.log('idk2')
+}else{
+    console.log('idk3')
+}
